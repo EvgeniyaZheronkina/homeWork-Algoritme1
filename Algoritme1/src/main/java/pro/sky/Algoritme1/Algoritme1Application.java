1ab1e27e1fa37ab52b98250503bef7e8333d55f6
zhero <zheronkina86@gmail.com>
2024-09-17 09:23:36 +0300

    New project

---
+++ Algoritme1/src/main/java/pro/sky/Algoritme1/Algoritme1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Algoritme1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algoritme1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Algoritme1Application.class, args);
+	}
+
+}
